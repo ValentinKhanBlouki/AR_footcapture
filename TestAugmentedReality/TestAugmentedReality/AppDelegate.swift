b1beedb8f5384a26eeb84e898f65fc2d34107df9
Knowing <knowing@MacBook-Pro-von-Knowing.local>
2023-05-12 15:12:14 +0200

    initial commit

---
+++ TestAugmentedReality/TestAugmentedReality/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  TestAugmentedReality
+//
+//  Created by Knowing on 02.05.23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
