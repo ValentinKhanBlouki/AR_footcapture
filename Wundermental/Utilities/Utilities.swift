22c8cedfc1379fc26c217069f068514685cfea15
nicolaswalter <nici.walter@hilver.de>
2023-05-20 16:51:36 +0200

    panning works

---
+++ Wundermental/Utilities/Utilities.swift
@@ -0,0 +1,16 @@
+//
+//  Utilities.swift
+//  Wundermental
+//
+//  Created by Nicolas Walter on 20.05.23.
+//
+
+import Foundation
+import SceneKit
+
+// Convenience accessors for Asset Catalog named colors.
+extension UIColor {
+    static let WYellow = UIColor(named: "WYellow")!
+    static let WGrey = UIColor(named: "WGrey")!
+
+}
--- Wundermental/ViewController.swift
+++ Wundermental/ViewController.swift
@@ -22,17 +22,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the view's delegate
         sceneView.delegate = self
         
+        // Create a new scene
+        let scene = SCNScene()
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         sceneView.addGestureRecognizer(tapGesture)
         
-        let moveGesture = UIPanGestureRecognizer(target: self, action: #selector(didPan(_:)))
-        view.addGestureRecognizer(moveGesture)
+        
+        let domeNode = Dome(radius: 0.3, horizontalSegments: 10, verticalSegments: 20, view: sceneView)
+        sceneView.scene.rootNode.addChildNode(domeNode)
+        domeNode.simdPosition = SIMD3<Float>(0, 0, 0)
 
-        let resizeGesture = UIPinchGestureRecognizer(target: self, action: #selector(didPinch(_:)))
-        view.addGestureRecognizer(resizeGesture)
         
     }
     
@@ -43,32 +49,6 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.run(configuration)
     }
     
-    @objc func didPan(_ gesture: UIPanGestureRecognizer) {
-        let location = gesture.location(in: sceneView)
-            let hits = self.sceneView.hitTest(location, options: nil)
-            if let tappedNode = hits.first?.node {
-                if gesture.state == .changed {
-                    let translation = gesture.translation(in: gesture.view)
-                    let modifiedPos = SCNVector3Make(tappedNode.position.x + Float(translation.x), tappedNode.position.y - Float(translation.y), tappedNode.position.z)
-                    tappedNode.position = modifiedPos
-                    gesture.setTranslation(CGPoint.zero, in: gesture.view)
-            }
-        }
-    }
-
-    @objc func didPinch(_ gesture: UIPinchGestureRecognizer) {
-        let location = gesture.location(in: sceneView)
-        let hits = self.sceneView.hitTest(location, options: nil)
-        if let tappedNode = hits.first?.node {
-            if gesture.state == .changed {
-                let pinchScaleX = Float(gesture.scale) * tappedNode.scale.x
-                let pinchScaleY = Float(gesture.scale) * tappedNode.scale.y
-                let pinchScaleZ = Float(gesture.scale) * tappedNode.scale.z
-                tappedNode.scale = SCNVector3(x: pinchScaleX, y: pinchScaleY, z: pinchScaleZ)
-                gesture.scale = 1
-            }
-        }
-    }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
@@ -91,69 +71,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             print("Object placement failed - couldn't find a surface")
         }
     }
-    
-    let radius: CGFloat = 0.3
-    let verticalSegments = 30
-    let horizontalSegments = 8
-    let height: CGFloat = 0.001
+
     
     // ARSCNViewDelegate
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let sphereNode = SCNNode()
+        return Dome(radius: 0.3, horizontalSegments: 10, verticalSegments: 20, view: sceneView)
 
-        for i in 0..<horizontalSegments {
-            let phi1 = CGFloat(i) * .pi / 2 / CGFloat(horizontalSegments - 1)
-            let phi2 = CGFloat(i+1) * .pi / 2 / CGFloat(horizontalSegments - 1)
-            
-            for j in 0..<verticalSegments {
-                let theta1 = CGFloat(j) * 2 * .pi / CGFloat(verticalSegments - 1)
-                let theta2 = CGFloat(j+1) *  2  * .pi / CGFloat(verticalSegments - 1)
-                
-                // Calculate the vertices of the rectangle
-                let vertex1 = SCNVector3(radius * sin(phi1) * cos(theta1), radius * cos(phi1), radius * sin(phi1) * sin(theta1))
-                let vertex2 = SCNVector3(radius * sin(phi1) * cos(theta2), radius * cos(phi1), radius * sin(phi1) * sin(theta2))
-                let vertex3 = SCNVector3(radius * sin(phi2) * cos(theta2), radius * cos(phi2), radius * sin(phi2) * sin(theta2))
-                let vertex4 = SCNVector3(radius * sin(phi2) * cos(theta1), radius * cos(phi2), radius * sin(phi2) * sin(theta1))
-                
-                // Create a geometry element for the rectangle
-                let vertices: [SCNVector3] = [vertex1, vertex2, vertex3, vertex4]
-                let vertexSource = SCNGeometrySource(vertices: vertices)
-                
-                if Bool.random() {
-                    // Fill the rectangle completely
-                    let indices: [UInt16] = [0, 1, 2, 2, 3, 0]
-                    let indexData = Data(bytes: indices, count: MemoryLayout<UInt16>.size * indices.count)
-                    let element = SCNGeometryElement(data: indexData, primitiveType: .triangles, primitiveCount: indices.count / 3, bytesPerIndex: MemoryLayout<UInt16>.size)
-                    let geometry = SCNGeometry(sources: [vertexSource], elements: [element])
-                    
-                    let material = SCNMaterial()
-                    material.diffuse.contents = UIColor.green
-                    geometry.materials = [material]
-                    
-                    let rectangleNode = SCNNode(geometry: geometry)
-                    sphereNode.addChildNode(rectangleNode)
-                } else {
-                    // Just draw the outline of the rectangle
-                    let indices: [UInt16] = [0, 1, 1, 2, 2, 3, 3, 0]
-                    let indexData = Data(bytes: indices, count: MemoryLayout<UInt16>.size * indices.count)
-                    let element = SCNGeometryElement(data: indexData, primitiveType: .line, primitiveCount: indices.count / 2, bytesPerIndex: MemoryLayout<UInt16>.size)
-                    let geometry = SCNGeometry(sources: [vertexSource], elements: [element])
-                    
-                    let material = SCNMaterial()
-                    material.diffuse.contents = UIColor.gray
-                    geometry.materials = [material]
-                    
-                    let rectangleNode = SCNNode(geometry: geometry)
-                    sphereNode.addChildNode(rectangleNode)
-                }
-                
-                if (i == 0) {
-                    break
-                }
-            }
-        }
-
-        return sphereNode
     }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
